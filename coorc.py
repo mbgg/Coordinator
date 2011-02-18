8361a2f38d97bca75487f3298558105dc4fd5c4a
Matthias Brugger <mensch0815@gmail.com>
2011-02-18 17:18:21 +0100

    tcp connection established basic fsm implemented

---
+++ coorc.py
@@ -0,0 +1,25 @@
+import socket
+
+print "coorc started"
+
+#ip = raw_input("IP-Adresse: ") 
+ip = '127.0.0.1'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((ip, 51000))
+s.send("start") 
+
+try: 
+	while True: 
+#		msg = raw_input("Nachricht: ") 
+		response = s.recv(1024) 
+		print "[%s] %s" % (ip,response) 
+		if response == "check":
+			s.send("rewind")
+			
+		elif response == "dorewind":
+			s.send("exit")
+			s.close()
+			break
+
+finally: 
+	s.close()
--- coord.py
+++ coord.py
@@ -1,6 +1,44 @@
 # coordinator which sends 
 #
 #
-i=100
-if i > 99:
-	print "test"
+
+import socket
+
+def communication():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	s.bind(("", 51000)) 
+	s.listen(1)
+
+	try: 
+		while True: 
+			komm, addr = s.accept() 
+			print "connected to %s" % addr[0]
+
+			while True: 
+				data = komm.recv(1024)
+
+				if not data: 
+					komm.close() 
+					break
+
+				#print "[%s] %s" % (addr[0], data) 	
+				if data == "start":
+					print "start recv"
+					komm.send("check") 
+
+				elif data == "rewind":
+					print "rewind recv"
+					komm.send("dorewind") 
+				
+				elif data == "exit":
+					komm.close()
+					s.close()
+					return
+
+	finally: 
+		s.close()
+
+print "coord started"
+communication()
+
+
