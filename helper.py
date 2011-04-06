cca6d3ec8eb7fd6be201583c9423b6dcadf82803
Matthias Brugger <mensch0815@gmail.com>
2011-04-06 19:25:31 +0200

    LogData class fully implemented. Testcases in postface

--- helper.py
+++ helper.py
@@ -4,36 +4,50 @@ class LogData:
 	
 	def __init__(self):#,pid,path,op,offset,size):
 		self.pidhash = {}
-#		filehash = {}
-#		rangelist = []
-#		rangelist.append([op, offset, size])
-#		filehash[path] = rangelist
-#		self.pidhash[pid] = filehash
 
-	def add(self, pid, path, op, offset, size):
+	def add(self, pid, path, op, offset, size): # add data to the data structure...
 		filehash = self.pidhash.setdefault(pid, {})
 		rangelist = filehash.setdefault(path, [])
 		rangelist.append([op, offset, size])
-		#print "%s added %s to %s [%d,%d]" %(pid, rangelist[len(rangelist)-1][0], filehash.keys(), offset, offset+size)
 
-	def get_ranges(self, pid, path):
+	def get_ranges(self, pid, path): # returns list of all file ranges from pid path tupel
 		filehash = self.pidhash.get(pid)
 		return filehash.get(path)
 		pass
 
-	def get_pids_all(self):
+	def get_pids_all(self): # returns all pids
 	  return self.pidhash.keys()
 
-	def get_pathes(self, pid):
+	def get_pathes(self, pid): # returns a list with all passes from given pid
 		fh = self.pidhash.get(pid)
 		return fh.keys()
 		pass
 
-	def get_pid_path(self):
-		pass
+	def get_pathes_all(self): # returns a list with all pathes
+		pathlist = []
+		for p in self.pidhash.itervalues():
+			for path in p.iterkeys():
+				if pathlist.count(path) == 0:
+					pathlist.append(path)
+		return pathlist
 
-	def get_pids(self, path):
-		pass
+	def get_pid_path(self): # returns a list with all ['pid', 'path'] values
+		pidpathlist = []
+		for p in self.pidhash.iterkeys():
+			f = self.pidhash.get(p)
+			for path in f.keys():
+				pidpathlist.append([p, path])
+		return pidpathlist
+
+	def get_pids(self, path): # returns a list of all pids by given path
+		pidlist = []
+		pids = self_pids_all()
+		for p in iter(pids):
+			filehash = self.pidhash.get(p)
+			if filehash.get(path) != None:
+				pidlist.append(pid)
+		return pidlist
+		
 
 	
 
--- log-checker.py
+++ log-checker.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python 
 #	The data structure:
 #
-#	hashmap pidhash(pid,filehash) -> all files used by pid
-# hashmap filehash(path,range) -> all byte ranges used for this file by pid
-# set     range([operation, offset, offset+size]+) -> ranges used by pid for specific file including operation
 #
 
 import sys
 import csv
-from hasht import HashTable
 from helper import LogData
 
 #conn = None #connection to hypervisor
 filename = '/tmp/fuse-log-fifo'
 global fd
-global pidhash 
 global logdata
 
 def log_checker_get_pid(something):
@@ -22,7 +17,7 @@ def log_checker_get_pid(something):
 	return 0
 
 def log_checker_preface():
-	global filename, fd, pidhash, logdata
+	global filename, fd, logdata
 
 	print "open %s for reading" %(filename)
 	#fd = csv.reader(open('/tmp/fuse-log-fifo', 'r'), delimiter=':', quotechar='|')
@@ -31,21 +26,24 @@ def log_checker_preface():
 		print "not able to open %s" %filename
 		return 1
 
-	pidhash = HashTable()
 	logdata = LogData()
 	
 
 def log_checker_postface():
 	print "entering postface"
 	global logdata
-	pidlist = logdata.get_pids_all()
 
+	pidlist = logdata.get_pids_all()
+	print "pidlist: %s" %pidlist
 	path = logdata.get_pathes(pidlist[0])
-	print logdata.get_ranges(pidlist[0], path[0])
-	print pidlist
-	for i in range(0, len(pidlist)):
-		print "pid %s accessed: %s" %(pidlist[i], logdata.get_pathes(pidlist[i]))
+	print "all ragnes from pid %s on file %s: %s" %(pidlist[0], path[0], logdata.get_ranges(pidlist[0], path[0]))
+	for i in iter(pidlist):
+		print "pid %s accessed: %s" %(i, logdata.get_pathes(i))
 	pass
+
+	print "all pathes are %s" %logdata.get_pathes_all()
+	pidpath = logdata.get_pid_path()
+	print "all [pid, path] tuples: %s" %pidpath
 
 def log_build_access_map(fd):
 	global logdata
@@ -57,42 +55,13 @@ def log_build_access_map(fd):
 		logdata.add(pid, path, op, offset, size)
 
 def log_minimize_map():
-	global pidhash
-
-	found = pidhash
-	#pidlist = []
-	#pidlist = pidhash.get_keys()
-
-	for pid in pidhash.get_keys(): #pidlist:
-		rangelist = []
-		filehash = pidhash.get(pid)
-		#filelist = []
-		#filelist = filehash.get_keys()
-
-		for f in filehash.get_keys(): #filelist:
-			rangelist = filehash.get(f)
-
-			while found != None:
-				found = None
-				for r in rangelist:
-					if offset >= r[1] and offset+size <= r[2] and op == r[0]:
-						print "%s op %s [%d,%d] already in filehash" %(pid, op, offset, offset+size)
-						found = r
-						break
-					# C + E
-					elif offset-1 >= r[1] and (offset-1 <= r[2])  and op == r[0]:
-						r[2] = offset+size
-						found = r
-						break 
-					# D + F
-
+	pass
 
 def find_conflicts():
 	pass
 
 if __name__ == "__main__":
 	global fd
-	global pidhash
 
 	print "log-checker started"
 	log_checker_preface()
